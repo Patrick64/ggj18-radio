fd9eb608cac4bef64c6106cf96fed14645f2250f
Patrick Woodcock <pmwoodcock@gmail.com>
2018-01-27 20:59:11 +0000

    added dial

--- GGJ Radio Unity/Assets/ControllerTuning.cs
+++ GGJ Radio Unity/Assets/ControllerTuning.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class ControllerTuning : MonoBehaviour {
     public Vector2? lastPosition = null;
     public Slider slider;
+    public Image dial;
     
     // Use this for initialization
     void Start () {
@@ -18,21 +19,38 @@ public class ControllerTuning : MonoBehaviour {
         float xAxis = Input.GetAxis("Horizontal");
         float yAxis = Input.GetAxis("Vertical");
         Vector2 newPosition = new Vector2(xAxis, yAxis);
-        if (this.lastPosition != null && xAxis != 0 && yAxis != 0)
+        if (xAxis != 0 && yAxis != 0)
         {
-            float angle = Vector2.SignedAngle((Vector2)lastPosition, newPosition)*0.04f;
-            if (angle > 0.7) angle = 0.7f;
-            if (angle < -0.7) angle = -0.7f;
-            float sliderChange = Time.deltaTime * angle;
-            
-            slider.value -= sliderChange;
+            if (this.lastPosition != null)
+            {
+                float gamePadAngle = Vector2.SignedAngle((Vector2)lastPosition, newPosition);
+                float angle = gamePadAngle * 0.04f;
+                if (angle > 0.7) angle = 0.7f;
+                if (angle < -0.7) angle = -0.7f;
+                float sliderChange = Time.deltaTime * angle;
+
+                slider.value -= sliderChange;
+                //float angleForDial = Vector2.SignedAngle(new Vector2(0,1), newPosition);
+                // dial.transform.rotation = Quaternion.Euler(0, 0, angleForDial);
+                dial.transform.rotation = Quaternion.Euler(0, 0, -slider.value * 360 * 5);
+            }
+            this.lastPosition = newPosition;
+        } else
+        {
+            float keySliderChange = Input.GetAxis("HorizontalKey") * 0.1f;
+            slider.value += keySliderChange * Time.deltaTime;
+            dial.transform.rotation = Quaternion.Euler(0, 0, -slider.value * 360 * 5);
+
         }
+
+
+
+
+
+
+
         
-        this.lastPosition = newPosition;
 
-
-        float keySliderChange = Input.GetAxis("HorizontalKey") * 0.1f;
-        slider.value += keySliderChange * Time.deltaTime;
 
     }
 }

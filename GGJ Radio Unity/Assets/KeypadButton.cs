9f710bcd5dc10fbd18254e2577e6bbcb81bcec41
Adam Carter-Groves <theuncommon@live.co.uk>
2018-01-27 18:40:54 +0000

    clickable keypad

--- GGJ Radio Unity/Assets/KeypadButton.cs
+++ GGJ Radio Unity/Assets/KeypadButton.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class KeypadButton : MonoBehaviour
 {
 	public Animator animator;
+	public int keyIndex;
 
 	private void Awake()
 	{
 		animator = GetComponent<Animator>();
+	}
+
+	public void Clicked()
+	{
+		//This is a horrible and I have no shame
+		transform.parent.parent.GetComponent<KeypadController>().KeyClicked(keyIndex);
 	}
 }

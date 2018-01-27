470e4c0ced07a90d08ac1d22cad38d8ba39db46e
Adam Carter-Groves <theuncommon@live.co.uk>
2018-01-27 11:04:16 +0000

    Testing stuff

---
+++ GGJ Radio Unity/Assets/TuningTester.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TuningTester : MonoBehaviour
+{
+	public AudioSource TestAudio;
+	public Slider TestSlider;
+	public float audioPosition;
+	public float tuningGap;
+	
+	void Update ()
+	{
+		float tuningDistance = Mathf.Abs(TestSlider.value - audioPosition);
+		if(tuningDistance < tuningGap)
+		{
+			float newVolume = (tuningGap - tuningDistance) / tuningGap;
+			TestAudio.volume = newVolume;
+			Debug.Log(newVolume);
+		}
+		else
+		{
+			TestAudio.volume = 0;
+		}
+	}
+}

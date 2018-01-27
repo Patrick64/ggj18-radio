96f7bf4a5b7c2562ff01ef2a2c1ff8e8ef220e85
Adam Carter-Groves <theuncommon@live.co.uk>
2018-01-27 16:00:38 +0000

    Multiple Stations, basic station deactivation

---
+++ GGJ Radio Unity/Assets/RadioStation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadioStation : MonoBehaviour
+{
+	public AudioSource radioSource;
+	public float audioPosition;
+	public float tuningGap;
+	public Slider TestSlider;
+	public string solution;
+	public bool isBroadcasting = false;
+
+	void Start ()
+	{
+		radioSource = GetComponent<AudioSource>();
+	}
+
+	public void ActivateAudio()
+	{
+		radioSource.Play();
+	}
+
+	public void DeactivateAudio()
+	{
+		radioSource.Stop();
+	}
+
+	void Update()
+	{
+		float tuningDistance = Mathf.Abs(TestSlider.value - audioPosition);
+		if(tuningDistance < tuningGap)
+		{
+			float newVolume = (tuningGap - tuningDistance) / tuningGap;
+			radioSource.volume = newVolume;
+		}
+		else
+		{
+			radioSource.volume = 0;
+		}
+	}
+}
---
+++ GGJ Radio Unity/Assets/StationController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationController : MonoBehaviour
+{
+	public RadioStation[] RadioStations;
+
+	private void OnEnable()
+	{
+		KeypadController.KeycodeEntered += SolutionSent;
+	}
+
+	private void OnDisable()
+	{
+		KeypadController.KeycodeEntered -= SolutionSent;
+	}
+
+	private void SolutionSent(string solution)
+	{
+		bool solutionFound = false;
+		for(int i = 0; i < RadioStations.Length; i++)
+		{
+			if(RadioStations[i].isBroadcasting && RadioStations[i].solution == solution)
+			{
+				solutionFound = true;
+				RadioStations[i].DeactivateAudio();
+				//ToDo: increment solution count
+				break;
+			}
+		}
+		
+		if(!solutionFound)
+		{
+			//Debug.Log("Solution Incorrect");
+		}
+	}
+
+	private void UpdateActiveStations()
+	{
+		//ToDo
+	}
+}

bc02138949f53ba6cf9cd42e67a5c7d8c73d4521
EloiseKnight97 <eloiseknight1997@gmail.com>
2018-01-27 20:15:00 +0000

    did anim stuff

---
+++ GGJ Radio Unity/Assets/StartScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void OnClick() {
+        SceneManager.LoadScene("testscene");
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
